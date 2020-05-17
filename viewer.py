080cbabb28ec22a064d1c42a2bc4da20c36b7a7c
Kaito32768 <suzukikaito14.ne.jp@gmail.com>
2020-05-17 15:07:30 +0900

    update some comment style

--- viewer.py
+++ viewer.py
@@ -1,39 +1,46 @@
-''' A simple OpenCV based viewer for dm_control images
-    from https://github.com/zuoxingdong/dm2gym
-'''
+"""
+A simple OpenCV based viewer for rendering
+from https://github.com/zuoxingdong/dm2gym
+"""
 import uuid
 import cv2
 
 
 class OpenCVImageViewer:
-    ''' A simple OpenCV highgui based dm_control image viewer
-        This class is meant to be a drop-in replacement for
-        `gym.envs.classic_control.rendering.SimpleImageViewer`
-    '''
-
+    """
+    A simple OpenCV highgui based dm_control image viewer
+    This class is meant to be a drop-in replacement for
+    `gym.envs.classic_control.rendering.SimpleImageViewer`
+    """
     def __init__(self, *, escape_to_exit=False):
-        ''' Construct the viewing window '''
+        """
+        Construct the viewing window
+        """
         self._escape_to_exit = escape_to_exit
         self._window_name = str(uuid.uuid4())
         cv2.namedWindow(self._window_name, cv2.WINDOW_AUTOSIZE)
         self._isopen = True
 
     def __del__(self):
-        ''' Close the window '''
+        """
+        Close the window
+        """
         cv2.destroyWindow(self._window_name)
         self._isopen = False
 
     def imshow(self, img):
-        ''' Show an image '''
-        # Convert image to BGR format
+        """
+        Show an image
+        """
         cv2.imshow(self._window_name, img[:, :, [2, 1, 0]])
-        # Listen for escape key, then exit if pressed
         if cv2.waitKey(1) in [27] and self._escape_to_exit:
             exit()
 
     @property
     def isopen(self):
-        ''' Is the window open? '''
+        """
+        Is the window open?
+        """
         return self._isopen
 
     def close(self):
