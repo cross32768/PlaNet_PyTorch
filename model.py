8443425e7c7b2e068c839de3a61d53c819b05781
Kaito32768 <suzukikaito14.ne.jp@gmail.com>
2020-05-17 15:31:42 +0900

    add docstring to model.py

--- model.py
+++ model.py
@@ -5,6 +5,9 @@ from torch.distributions import Normal
 
 
 class Encoder(nn.Module):
+    """
+    Encoder to embed image observation (3, 64, 64) to vector (1024,)
+    """
     def __init__(self):
         super(Encoder, self).__init__()
         self.cv1 = nn.Conv2d(3, 32, kernel_size=4, stride=2)
@@ -21,6 +24,11 @@ class Encoder(nn.Module):
 
 
 class ObservationModel(nn.Module):
+    """
+    p(o_t | s_t, h_t)
+    Observation model to reconstruct image observation (3, 64, 64)
+    from state and rnn hidden state
+    """
     def __init__(self, state_dim, rnn_hidden_dim):
         super(ObservationModel, self).__init__()
         self.fc = nn.Linear(state_dim + rnn_hidden_dim, 1024)
@@ -40,6 +48,10 @@ class ObservationModel(nn.Module):
 
 
 class RewardModel(nn.Module):
+    """
+    p(r_t | s_t, h_t)
+    Reward model to predict reward from state and rnn hidden state
+    """
     def __init__(self, state_dim, rnn_hidden_dim, hidden_dim=200):
         super(RewardModel, self).__init__()
         self.fc1 = nn.Linear(state_dim + rnn_hidden_dim, hidden_dim)
@@ -54,6 +66,13 @@ class RewardModel(nn.Module):
 
 
 class RecurrentStateSpaceModel(nn.Module):
+    """
+    This class includes multiple components
+    Deterministic state model: h_t+1 = f(h_t, s_t, a_t)
+    Stochastic state model (prior): p(s_t+1 | h_t+1)
+    State posterior: q(s_t | h_t, o_t)
+    NOTE: actually, this class takes embedded observation by Encoder class
+    """
     def __init__(self, state_dim, action_dim, rnn_hidden_dim,
                  hidden_dim=200, min_stddev=0.1):
         super(RecurrentStateSpaceModel, self).__init__()
@@ -70,14 +89,21 @@ class RecurrentStateSpaceModel(nn.Module):
         self.rnn = nn.GRUCell(hidden_dim, rnn_hidden_dim)
         self._min_stddev = min_stddev
 
-    def forward(self, state, action, rnn_hidden, embedded_next_obs=None):
+    def forward(self, state, action, rnn_hidden, embedded_next_obs):
+        """
+        h_t+1 = f(h_t, s_t, a_t)
+        Return prior p(s_t+1 | h_t+1) and posterior p(s_t+1 | h_t+1, o_t+1)
+        for model training
+        """
         next_state_prior, rnn_hidden = self.prior(state, action, rnn_hidden)
-        if embedded_next_obs is None:
-            return next_state_prior, None, rnn_hidden
         next_state_posterior = self.posterior(rnn_hidden, embedded_next_obs)
         return next_state_prior, next_state_posterior, rnn_hidden
 
     def prior(self, state, action, rnn_hidden):
+        """
+        h_t+1 = f(h_t, s_t, a_t)
+        Compute prior p(s_t+1 | h_t+1)
+        """
         hidden = F.elu(self.fc_state_action(torch.cat([state, action], dim=1)))
         rnn_hidden = self.rnn(hidden, rnn_hidden)
         hidden = F.elu(self.fc_rnn_hidden(rnn_hidden))
@@ -87,6 +113,9 @@ class RecurrentStateSpaceModel(nn.Module):
         return Normal(mean, stddev), rnn_hidden
 
     def posterior(self, rnn_hidden, embedded_next_obs):
+        """
+        Compute posterior q(s_t | h_t, o_t)
+        """
         hidden = F.elu(self.fc_rnn_hidden_next_embedded_obs(
             torch.cat([rnn_hidden, embedded_next_obs], dim=1)))
         mean = self.fc_next_state_mean_posterior(hidden)
