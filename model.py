53300e6eb0b9bead197d7953b0f087eb42fa6cfe
Kaito32768 <suzukikaito14.ne.jp@gmail.com>
2020-05-19 14:45:32 +0900

    update activation function same as original implementation

--- model.py
+++ model.py
@@ -31,10 +31,10 @@ class RecurrentStateSpaceModel(nn.Module):
     State posterior: q(s_t | h_t, o_t)
     NOTE: actually, this class takes embedded observation by Encoder class
     min_stddev is added to stddev same as original implementation
-    Activation function for this class is F.elu same as original implementation
+    Activation function for this class is F.relu same as original implementation
     """
     def __init__(self, state_dim, action_dim, rnn_hidden_dim,
-                 hidden_dim=200, min_stddev=0.1):
+                 hidden_dim=200, min_stddev=0.1, act=F.relu):
         super(RecurrentStateSpaceModel, self).__init__()
         self.state_dim = state_dim
         self.action_dim = action_dim
@@ -48,6 +48,7 @@ class RecurrentStateSpaceModel(nn.Module):
         self.fc_state_stddev_posterior = nn.Linear(hidden_dim, state_dim)
         self.rnn = nn.GRUCell(hidden_dim, rnn_hidden_dim)
         self._min_stddev = min_stddev
+        self.act = act
 
     def forward(self, state, action, rnn_hidden, embedded_next_obs):
         """
@@ -64,9 +65,9 @@ class RecurrentStateSpaceModel(nn.Module):
         h_t+1 = f(h_t, s_t, a_t)
         Compute prior p(s_t+1 | h_t+1)
         """
-        hidden = F.elu(self.fc_state_action(torch.cat([state, action], dim=1)))
+        hidden = self.act(self.fc_state_action(torch.cat([state, action], dim=1)))
         rnn_hidden = self.rnn(hidden, rnn_hidden)
-        hidden = F.elu(self.fc_rnn_hidden(rnn_hidden))
+        hidden = self.act(self.fc_rnn_hidden(rnn_hidden))
 
         mean = self.fc_state_mean_prior(hidden)
         stddev = F.softplus(self.fc_state_stddev_prior(hidden)) + self._min_stddev
@@ -76,7 +77,7 @@ class RecurrentStateSpaceModel(nn.Module):
         """
         Compute posterior q(s_t | h_t, o_t)
         """
-        hidden = F.elu(self.fc_rnn_hidden_embedded_obs(
+        hidden = self.act(self.fc_rnn_hidden_embedded_obs(
             torch.cat([rnn_hidden, embedded_obs], dim=1)))
         mean = self.fc_state_mean_posterior(hidden)
         stddev = F.softplus(self.fc_state_stddev_posterior(hidden)) + self._min_stddev
